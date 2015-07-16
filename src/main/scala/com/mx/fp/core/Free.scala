d2da8063db4f5ed7630504129b6aee89c51dea80
MiloXia <1035708003@qq.com>
2015-07-16 22:16:28 +0800

    add free monad

--- src/main/scala/com/mx/fp/core/Free.scala
+++ src/main/scala/com/mx/fp/core/Free.scala
@@ -2,41 +2,40 @@ package com.mx.fp.core
 
 import scala.xml.{XML, NodeSeq}
 
-object NewsLetterOpTest extends App {
-  trait Free[F[_],A] {
-    def unit(a: A): Free[F,A] = Return(a)
-    def flatMap[B](k: A => Free[F,B]): Free[F,B] = this match {
+trait Free[F[_],A] {
+  def unit(a: A): Free[F,A] = Return(a)
+  def flatMap[B](k: A => Free[F,B]): Free[F,B] = this match {
+    case Return(a) =>
+      k(a)
+    case Bind(command, cont) =>
+      Bind(command, cont andThen (_ flatMap k))
+  }
+  def map[B](f: A => B): Free[F,B] =
+    flatMap(f andThen (Return(_)))
+
+  def foldMap[G[_]: Monad](f: F ~> G): G[A] = {
+    val G = implicitly[Monad[G]]
+    this match {
       case Return(a) =>
-        k(a)
+        G.unit(a)
       case Bind(command, cont) =>
-        Bind(command, cont andThen (_ flatMap k))
-    }
-    def map[B](f: A => B): Free[F,B] =
-      flatMap(f andThen (Return(_)))
-
-    def foldMap[G[_]: Monad](f: F ~> G): G[A] = {
-      val G = implicitly[Monad[G]]
-      this match {
-        case Return(a) =>
-          G.unit(a)
-        case Bind(command, cont) =>
-          G.flatMap(f(command))(cont andThen (_ foldMap f))
-      }
+        G.flatMap(f(command))(cont andThen (_ foldMap f))
     }
   }
-  final case class Return[F[_], A](a: A) extends Free[F,A]
-  final case class Bind[F[_], R, A](command: F[R],
-                                    cont: R => Free[F,A]) extends Free[F,A]
+}
+final case class Return[F[_], A](a: A) extends Free[F,A]
+final case class Bind[F[_], R, A](command: F[R],
+                                  cont: R => Free[F,A]) extends Free[F,A]
 
-  trait ~>[F[_],G[_]] {
-    def apply[A](fa: F[A]): G[A]
-  }
+trait ~>[F[_],G[_]] {
+  def apply[A](fa: F[A]): G[A]
+}
 
-  object Free {
-    def lift[F[_],R](command: F[R]): Free[F,R] =
-      Bind(command, (x:R) => Return(x))
-  }
-
+object Free {
+  def lift[F[_],R](command: F[R]): Free[F,R] =
+    Bind(command, (x:R) => Return(x))
+}
+object NewsLetterOpTest extends App {
 
   trait NewsLetterOp[A]
   case class GetSubject(s: String) extends NewsLetterOp[String]
@@ -50,11 +49,6 @@ object NewsLetterOpTest extends App {
   implicit def liftNewsLetter[A](n: NewsLetterOp[A]): FreeNewsLetter[A] = Free.lift(n)
 
   //Id
-  trait Monad[F[_]] {
-    def unit[A](a: A): F[A]
-    def flatMap[A,B](m: F[A])(k: A => F[B]): F[B]
-    def map[A,B](m: F[A])(f: A => B): F[B] = flatMap(m)(f andThen unit)
-  }
   sealed case class Id[A](a: A)
   implicit val IdMonad: Monad[Id] = new Monad[Id] {
     def unit[A](a: A) = Id(a)
