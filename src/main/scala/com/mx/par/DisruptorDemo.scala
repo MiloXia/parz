0c06285c04fb4df86a6462ec80cda5bdc41c12df
milo <xiaziyao@qiaobutang.com>
2015-08-03 18:07:34 +0800

    modify disruptor .

--- src/main/scala/com/mx/par/DisruptorDemo.scala
+++ src/main/scala/com/mx/par/DisruptorDemo.scala
@@ -5,33 +5,34 @@ import java.util.concurrent.Executors
 
 import com.lmax.disruptor.dsl.Disruptor
 import com.lmax.disruptor.{RingBuffer, EventHandler, EventFactory}
-import com.mx.par.day06.{Get, GetUp, Request}
+import com.mx.par.day06.Request
 
 /**
  * Created by milo on 15-7-30.
  */
 object DisruptorDemo {
 
-  class RequestEvent {
-    var event: Request[_] = _
-    def set(r: Request[_]) { event = r }
+  trait EventEnvelope[Event[_]] {
+    var event: Event[_] = _
+    def set(r: Event[_]) { event = r }
   }
+
+  trait GotoWork[A] extends Request[A]
+  case object GetUp extends GotoWork[Unit]
+
+  trait IOReq[A] extends Request[A]
+  case class Get(q: String) extends IOReq[String]
+  case class Put(q: String) extends IOReq[Unit]
+
+  class GotoWorkEvent extends EventEnvelope[GotoWork]
+  class IOReqEvent extends  EventEnvelope[IOReq]
 
   def eventFactory[Event](e: => Event) = new EventFactory[Event] {
     def newInstance(): Event = e
   }
 
-  class LongEventHandler extends EventHandler[RequestEvent] {
-    def onEvent(event: RequestEvent, sequence: Long, endOfBatch: Boolean) {
-      event.event match {
-        case GetUp => println("Event: GetUp")
-        case Get(q) => println("Event: Get " + q)
-      }
-    }
-  }
-
-  class LongEventProducer(val ringBuffer: RingBuffer[RequestEvent]) {
-    def onData[A](event: Request[A]) = {
+  class LongEventProducer(val ringBuffer: RingBuffer[GotoWorkEvent]) {
+    def onData(event: GotoWork[_]) = {
       val sequence = ringBuffer.next()
       try {
         val reqEvent = ringBuffer.get(sequence)
@@ -42,17 +43,59 @@ object DisruptorDemo {
     }
   }
 
+  class LongEventProducer2(val ringBuffer: RingBuffer[IOReqEvent]) {
+    def onData(event: IOReq[_]) = {
+      val sequence = ringBuffer.next()
+      try {
+        val reqEvent = ringBuffer.get(sequence)
+        reqEvent.set(event)
+      } finally {
+        ringBuffer.publish(sequence)
+      }
+    }
+  }
+
+  class LongEventHandler extends EventHandler[GotoWorkEvent] {
+    def onEvent(event: GotoWorkEvent, sequence: Long, endOfBatch: Boolean) {
+      event.event match {
+        case GetUp => println("Event: GetUp")
+
+      }
+    }
+  }
+
+  class LongEventHandler2 extends EventHandler[IOReqEvent] {
+    def onEvent(event: IOReqEvent, sequence: Long, endOfBatch: Boolean) {
+      event.event match {
+        case Get(q) => println("Event: Get " + q)
+        case Put(p) => println("Event: Put" + p)
+      }
+    }
+  }
+
   def main(args: Array[String]) {
     val executor = Executors.newCachedThreadPool()
     val bufferSize = 1024
-    val disruptor = new Disruptor[RequestEvent](eventFactory{new RequestEvent()}, bufferSize, executor)
+
+    val disruptor = new Disruptor[GotoWorkEvent](eventFactory{new GotoWorkEvent()}, bufferSize, executor)
     disruptor.handleEventsWith(new LongEventHandler())
     disruptor.start()
+
+    val disruptor2 = new Disruptor[IOReqEvent](eventFactory{new IOReqEvent()}, bufferSize, executor)
+    disruptor2.handleEventsWith(new LongEventHandler2())
+    disruptor2.start()
+
     val ringBuffer = disruptor.getRingBuffer()
     val producer = new LongEventProducer(ringBuffer)
+
+    val ringBuffer2 = disruptor2.getRingBuffer()
+    val producer2 = new LongEventProducer2(ringBuffer2)
+
     for (l <- 1 to 1000) {
-      producer.onData(GetUp)
-      producer.onData(Get("a"))
+      val l1: List[IOReq[_]] = List(Get("a"), Put("p"))
+      val l2: List[GotoWork[_]] = List(GetUp)
+      l1.foreach(producer2.onData)
+      l2.foreach(producer.onData)
       Thread.sleep(100)
     }
   }
